790f9c7b1bac033dee7df68ada57dd603d65d708
Jaspreet Mahal <jpjaspreetmahal@Jaspreets-MacBook-Pro.local>
2017-11-08 16:35:17 +0530

    Finish 1

--- designs.js
+++ designs.js
@@ -2,23 +2,33 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid() {
+  $('table').empty();
 
-$(document).ready( function(){
-  $('#sizepicker').submit(makeGrid())
+  var height = $('#input_height').val();
+  var width = $('#input_width').val();
+  var colorPicker = $('#colorPicker');
+  var table,row;
 
-  function makeGrid() {
-    var height = $('#input_height').val();
-    var width = $('#input_width').val();
-    console.log(height);
-    var table,row;
-    for (var i = 0; i < height; i++) {
-      table = $('#pixel_canvas');
-      table.append('<tr class="row"></tr>')
-      row = $('.row').last();
-      for (var j = 0; j < width; j++) {
-        row.append('<td></td>');
-      }
+  for (var i = 0; i < height; i++) {
+    table = $('#pixel_canvas');
+    table.append('<tr class="row"></tr>')
+    row = $('.row').last();
+    for (var j = 0; j < width; j++) {
+      row.append('<td></td>');
     }
   }
 
-});
+  $('td').click( function(){
+    this.style.backgroundColor = colorPicker.val();
+  } );
+
+  $('td').dblclick( function(){
+    this.style.backgroundColor = "";
+  } );
+
+}
+
+function clearGrid(){
+  $('table').empty();
+}
